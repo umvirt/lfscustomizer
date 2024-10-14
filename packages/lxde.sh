09627087f1e816bf36c4d5669a59ed078a9346b7
UmVirt development team <dev@umvirt.com>
2024-10-14 06:19:59 +0000

    update packages sets installation scripts

--- packages/lxde.sh
+++ packages/lxde.sh
@@ -1,5 +1,8 @@
 . /etc/profile
+if [ -f /sources/uname.sh ]
+then
 . /sources/uname.sh
+fi
 
 uname -m
 sleep 30
--- packages/x.sh
+++ packages/x.sh
@@ -1,5 +1,9 @@
 . /etc/profile
+
+if [ -f /sources/uname.sh ]
+then
 . /sources/uname.sh
+fi
 
 uname -m
 sleep 30
