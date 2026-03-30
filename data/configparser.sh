629237716584b090981b387070ff05f33ea3f0b5
UmVirt development team <dev@umvirt.com>
2026-03-30 14:57:57 +0900

    New configuration file options for MBR boot record update.

--- data/configparser.sh
+++ data/configparser.sh
@@ -1,9 +1,15 @@
 if [[ $LFS_PARTITION == "" ]]
 then
-    echo "ERROR: Target partition is not defined"
-    exit
+LFS_PARTITION='/dev/sda1'
 fi
-
+if [[ $LFS_RUNNING_PARTITION == "" ]]
+then
+LFS_RUNNING_PARTITION='/dev/sda1'
+fi
+if [[ $LFS_RUNNING_DISK_NUMBER == "" ]]
+then
+LFS_RUNNING_DISK_NUMBER=0
+fi
 if [[ $LFS_TARGET_ARCH == "" ]]
 then
 LFS_TARGET_ARCH='amd64'
@@ -22,4 +28,11 @@ if [[ $ZEROFILL == "" ]]
 then
 ZEROFILL='YES'
 fi
+if [[ $LFS_MBR == "" ]]
+then
+LFS_MBR='YES'
+fi
 
+LFS_DISK=`echo $LFS_PARTITION | sed -e 's/[0-9]*$//'`
+LFS_RUNNING_DISK=`echo $LFS_RUNNING_PARTITION | sed -e 's/[0-9]*$//'`
+LFS_RUNNING_PARTITION_NUMBER=`echo $LFS_RUNNING_PARTITION | sed -e "s|$LFS_RUNNING_DISK||"`
